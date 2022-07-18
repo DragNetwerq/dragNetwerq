80857bb05fc7fc1ecc039008b67de1abfb0a4cb1
GabriellaPenev <gabriellapenev@gmail.com>
2022-07-18 18:12:59 -0400

    Styled the results section

--- scripts.js
+++ scripts.js
@@ -10,7 +10,6 @@ queenApp.init = () => {
 
 queenApp.url = 'http://www.nokeynoshade.party/api/queens?limit=8'
 
-
 // method to call the api
 queenApp.getQuotes = () => {
     const url = new URL(queenApp.url);
@@ -19,7 +18,7 @@ queenApp.getQuotes = () => {
             return data.json();
         })
         .then(function (jsonData) {
-            // console.log(jsonData);
+            console.log(jsonData);
             // we now have the json data we can work with:
             // call the displayQuote method here:
             queenApp.displayQuotes(jsonData);
@@ -30,21 +29,20 @@ queenApp.getQuotes = () => {
 //Method to display data on the page and pass it in a parameter so that the method knows it'll have to actually accept an argument when it's called
 
 // Display a random quote in the button field - Create function called displayQuote that will pull the quote and display it upon pageload
-
-
-
-
 queenApp.displayQuotes = (queens) => {
 
     // ***** NEED TO GET RANDOMIZER TO ACTUALLY APPLY TO THE QUEEN ARRAY FROM API *****
-    getRandomQueen = (queens) => {
-        const randomQueen = Math.floor(Math.random() * queens.length);
-
-        return queens[randomQueen];
-    }
-
-    console.log(queens);
+    // getRandomQueen = (randQueens) => {
+    //     const randomQueen = Math.floor(Math.random() * randQueens.length);
+    // console.log(randomQueen);
+    //     // return queens[randomQueen];
+    //     queenApp.individualQueen = queens[randomQueen];
+    // }
     
+    // getRandomQueen(queens);
+    // console.log(queenApp.individualQueen);
+    
+
     const ulElement = document.querySelector('.displayData');
 
     //for each quote in the quote array, run some code:
@@ -56,8 +54,11 @@ queenApp.displayQuotes = (queens) => {
 
         ulElement.appendChild(liElement);
         liElement.appendChild(buttonElement);
-        buttonElement.appendChild(quoteParagraph);
-        console.log(buttonElement);
+        
+        if(quoteParagraph != ' ' ){
+            buttonElement.appendChild(quoteParagraph);
+        }
+        // console.log(buttonElement);
         
         // add innerText to the quoteParagraph variable, equal to the queen object's 'quote' property:
         quoteParagraph.innerText = queen.quote;
@@ -66,6 +67,7 @@ queenApp.displayQuotes = (queens) => {
         // Make an event listener method for when user clicks the button and store the user’s selection in a variable
         buttonElement.addEventListener('click', function(){
         
+        // clear the results section of any data on click:
         const results = document.querySelector('.results');
         results.innerText = '';
 
@@ -75,39 +77,23 @@ queenApp.displayQuotes = (queens) => {
         imageElement.alt = `Drag Queen ${queen.name}`;
         
         // create the queen name:
-        const queenName = document.createElement('p');
+        const queenName = document.createElement('h3');
         queenName.innerText = queen.name;
 
         // create the queen season number:
         const seasonNum = document.createElement('p');
-        seasonNum.innerText = queen.seasons.seasonNumber;
+        seasonNum.innerText = `Appeared on Season # ${queen.seasons[0].seasonNumber}`;
         
-        
-        results.appendChild(queenName);
-        results.appendChild(seasonNum);
-        results.appendChild(imageElement);
+        results.append(queenName, seasonNum, imageElement);
 
         })
-
     })
     
 }
 
-
-
-
-
-
-
 // Return / display the selected Drag Queen profile, passing in the user’s choice to retrieve the related Queen’s name, image, and season’s they were on Drag Race from the API
 
 // Ask the user if they want to pick another quote / ie refresh the page and show different quotes - figure out how to clear the data from the api call, rather than refreshing the page(on button click) 
-
-
-
-
-
-
 
 queenApp.init();
 // Call the init at the end of the file
