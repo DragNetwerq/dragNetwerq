a6d2cc1bba2f0676622c5f56741ed1cf8c9a6b5a
GabriellaPenev <gabriellapenev@gmail.com>
2022-07-22 14:58:22 -0400

    Added dropdown seasons menu to index.html

--- scripts.js
+++ scripts.js
@@ -1,14 +1,12 @@
-// Create a namespace
+// Come up with namespace name for our app, 'queenApp' and store it in a variable:
 const queenApp = {};
 
 queenApp.init = () => {
     // Store functions on pageload in init
     queenApp.getQuotes();
-    
-
 }
 
-queenApp.url = 'http://www.nokeynoshade.party/api/queens?limit=8'
+queenApp.url = 'http://www.nokeynoshade.party/api/queens?limit=9'
 
 // method to call the api
 queenApp.getQuotes = () => {
@@ -18,53 +16,38 @@ queenApp.getQuotes = () => {
             return data.json();
         })
         .then(function (jsonData) {
-            console.log(jsonData);
+            // console.log(jsonData);
             // we now have the json data we can work with:
-            // call the displayQuote method here:
+            // call the displayQuote method here, and pass in the data as the argument:
             queenApp.displayQuotes(jsonData);
         })
 };
 
-
 //Method to display data on the page and pass it in a parameter so that the method knows it'll have to actually accept an argument when it's called
-
-// Display a random quote in the button field - Create function called displayQuote that will pull the quote and display it upon pageload
+// Display a quote in the button field - Create function called displayQuote that will pull the quote and display it upon pageload
 queenApp.displayQuotes = (queens) => {
-
-    // ***** NEED TO GET RANDOMIZER TO ACTUALLY APPLY TO THE QUEEN ARRAY FROM API *****
-    // getRandomQueen = (randQueens) => {
-    //     const randomQueen = Math.floor(Math.random() * randQueens.length);
-    // console.log(randomQueen);
-    //     // return queens[randomQueen];
-    //     queenApp.individualQueen = queens[randomQueen];
-    // }
-    
-    // getRandomQueen(queens);
-    // console.log(queenApp.individualQueen);
-    
 
     const ulElement = document.querySelector('.displayData');
 
-    //for each quote in the quote array, run some code:
+    //for each queen in the array, run the following code:
     queens.forEach(queen => {
         const liElement = document.createElement('li');
         const buttonElement = document.createElement('button');
         const quoteParagraph = document.createElement('p');   
         // console.log(queen);
 
-        ulElement.appendChild(liElement);
-        liElement.appendChild(buttonElement);
-        
-        if(quoteParagraph != ' ' ){
+        if(queen.quote != '""'){
+            ulElement.appendChild(liElement);
+            liElement.appendChild(buttonElement);
             buttonElement.appendChild(quoteParagraph);
         }
         // console.log(buttonElement);
         
         // add innerText to the quoteParagraph variable, equal to the queen object's 'quote' property:
         quoteParagraph.innerText = queen.quote;
-        // console.log(quoteParagraph);
+        // console.log(queen.quote);
 
-        // Make an event listener method for when user clicks the button and store the user’s selection in a variable
+        // Make an event listener method for when user clicks the button:
         buttonElement.addEventListener('click', function(){
         
         // clear the results section of any data on click:
@@ -76,24 +59,19 @@ queenApp.displayQuotes = (queens) => {
         imageElement.src = queen.image_url;
         imageElement.alt = `Drag Queen ${queen.name}`;
         
-        // create the queen name:
+        // create the queen name, add the name from the queen object to the new variable:
         const queenName = document.createElement('h3');
         queenName.innerText = queen.name;
 
-        // create the queen season number:
+        // create the queen season number, add the season number from the queen object to the new variable:
         const seasonNum = document.createElement('p');
         seasonNum.innerText = `Appeared on Season # ${queen.seasons[0].seasonNumber}`;
         
         results.append(queenName, seasonNum, imageElement);
-
         })
     })
     
 }
-
-// Return / display the selected Drag Queen profile, passing in the user’s choice to retrieve the related Queen’s name, image, and season’s they were on Drag Race from the API
-
-// Ask the user if they want to pick another quote / ie refresh the page and show different quotes - figure out how to clear the data from the api call, rather than refreshing the page(on button click) 
 
 queenApp.init();
 // Call the init at the end of the file
